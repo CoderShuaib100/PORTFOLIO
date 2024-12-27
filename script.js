cfe4706a82af9e31a00ed4ee9480b1d01a326bfe
CoderShuaib100 <sayeedishuaib@gmail.com>
2024-12-27 19:32:22 +0530

    15%

--- script.js
+++ script.js
@@ -5,7 +5,41 @@
         loop:true
     });
 
+// creating the motherboard for the digital clock
 
 
 
+const monthNames = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+const dayNames = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+function setTime(){
+    var date = new Date();
+    var h = date.getHours();
+    var min = date.getMinutes();
+    var d = date.getDate();
+    var m = date.getMonth();
+    var y = date.getFullYear();
+    var day = date.getDay();
+    var text = document.getElementById("timetext");
+    var s = date.getSeconds();
+    session = "AM";
+    if (h > 12) { 
+        h = h - 12; session = "PM"; 
+    } 
+    else if (h === 12) { 
+        session = "PM"; 
+    } 
+    else if (h === 0) { 
+        h = 12;
+    }
+    min = min < 10 ? "0" + min : min; 
+    h = h < 10 ? "0" + h : h;
+    s = s < 10 ? "0" + s : s;
+    m = monthNames[m];
+    day = dayNames[day];
+
+    text.innerHTML = h + ":" + min + " " + session + "<br>" + d + "/" + m + "/" + y + "<br>" + day + "<br>" + s + " seconds";
     
+}   
+setInterval(setTime,1000);
+setTime();
